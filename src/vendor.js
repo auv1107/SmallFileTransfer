111f4c9bce7a162f2941ff3e48062b23a353001e
antiless <antiless@163.com>
2018-05-06 10:11:37 +0800

    Import vue modules partly.
    Signed-off-by: antiless <antiless@163.com>

--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,14 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import { Button, Container, Header, Main, Tabs, TabPane, Upload, Input, Table, TableColumn, Footer} from 'element-ui'
+
+Vue.use(Button);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Main);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Upload);
+Vue.use(Input);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Footer);
